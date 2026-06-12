8b2c2f97c6b5fe168e610ffb419a0b718e1f5489
Sunil Nagrale <Sunil@3225>
2026-06-12 23:53:59 +0530

    Fix login using email field

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,25 @@
+// const passport = require('passport');
+// const LocalStrategy = require('passport-local');
+// const User = require('../models/User');
+
+// passport.use(new LocalStrategy(User.authenticate()));
+
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// module.exports = passport;
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/User');
 
-passport.use(new LocalStrategy(User.authenticate()));
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email'
+    },
+    User.authenticate()
+  )
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
